a42db38f7e0480bfd9c443ad78a5c2070d0fc66d
Faysal Ahamed <miufaysal@live.com>
2019-08-01 14:11:28 +0600

    wip

--- resources/views/members/form.blade.php
+++ resources/views/members/form.blade.php
@@ -18,16 +18,16 @@
     <p class="text-center pt-5"><span class="inline-block bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Membership Application Form</span></p>
     <img id="profileImage" class="h-40 w-40 mx-auto" src="{{asset('/images/placeholder-person.png')}}" alt="">
 </div>
-<div class="pb-3">
-    @if (session('status'))
+@if (session('status'))
+    <div class="pb-3">
         <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
             <span class="block sm:inline">{{ session('status') }}</span>
             <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
                 <svg class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
             </span>
         </div>
-    @endif
-</div>
+    </div>
+@endif
 <div class="flex flex-wrap -mx-3 mb-0 md:mb-4">
     <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
         <label class="miu-label" for="name">
