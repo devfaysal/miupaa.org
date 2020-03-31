ae772e5dc8cf7636d102aff77832985dd8a4137d
Faysal Ahamed <miufaysal@live.com>
2020-03-31 06:37:39 +0600

    wip

--- resources/views/testing.blade.php
+++ resources/views/testing.blade.php
@@ -9,11 +9,31 @@
 <body>
     <form action="" method="post" enctype="multipart/form-data">
         @csrf
-        <input type="file" accept="image/*" name="image" capture="camera">
+        <input type="file" id="image" accept="image/*" name="image" capture="camera">
         <input type="submit" value="Upload">
     </form>
 
     <h1>Image</h1>
-    <img src="{{asset('/storage/'. $imageName)}}" alt="">
+    <img id="imageView" style="max-width: 200px;" src="{{asset('/storage/'. $imageName)}}" alt="">
+
+    <script>
+        input = document.querySelector('#image');
+        
+        input.addEventListener('change', function(){
+            preview = document.querySelector('#imageView');
+            file    = input.files[0];
+            reader  = new FileReader();
+
+            reader.onloadend = function () {
+                preview.src = reader.result;
+            }
+
+            if (file) {
+                reader.readAsDataURL(file);
+            } else {
+                preview.src = "";
+            }
+        });
+    </script>
 </body>
 </html>
